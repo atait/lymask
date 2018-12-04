cb52b5f8deddd482f612337132ab405a9322629f
Alex Tait <alexander.tait@nist.gov>
2018-12-03 23:30:54 -0700

    packaging and CI deployment

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,11 +1,14 @@
-from lymask import batch_main
-from lytest import run_xor
+import os
 import subprocess
 
-dataprep_file = 'example-dataprep.yml'
-layout_file = 'example_src.oas'
-outfile = 'example_run.oas'
-reffile = 'example_answer.oas'
+from lymask import batch_main
+from lytest import run_xor
+
+test_dir = os.path.dirname(__file__)
+dataprep_file = os.path.join(test_dir, 'example-dataprep.yml')
+layout_file = os.path.join(test_dir, 'example_src.oas')
+outfile = os.path.join(test_dir, 'example_run.oas')
+reffile = os.path.join(test_dir, 'example_answer.oas')
 
 
 # This one need Technology working
