aea1015fbb9e2571e7966c58bf5882312b82a043
Alex Tait <alexander.tait@nist.gov>
2019-06-21 16:47:17 -0600

    version bump 0.1.0

--- lymask/__init__.py
+++ lymask/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.0.9'
+__version__ = '0.1.0'
 __lygadget_link__ = ['lygadgets', 'yaml']
 
 from lymask.utilities import active_technology, set_active_technology
--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ def readme():
 
 
 setup(name='lymask',
-      version='0.0.9',
+      version='0.1.0',
       description='Mask dataprep with klayout',
       long_description=readme(),
       author='Alex Tait, Adam McCaughan, Sonia Buckley, Jeff Chiles, Jeff Shainline, Rich Mirin, Sae Woo Nam',
       author_email='alexander.tait@nist.gov',
       license='MIT',
       packages=['lymask'],
-      install_requires=['klayout', 'lygadgets>=0.1.14', 'PyYAML'],
+      install_requires=['klayout', 'lygadgets', 'PyYAML'],
       entry_points={'console_scripts': ['lymask=lymask.invocation:cm_main']},
       package_data={'': ['*.lym']},
       include_package_data=True,
